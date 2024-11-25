5cefb7f03bf9e3d011e461dddeb2041ba4039d85
wen A <florezaraujo@hotmail.com>
2024-11-24 20:59:55 -0500

    se crea ruta en swagger

---
+++ docs/swagget.ts
@@ -0,0 +1,23 @@
+export const swaggerOptions = {
+    
+        openapi:"3.0.3",
+        info:{
+            title: "Api reconocimiento de texto",
+            description: "Micro servicio de reconocimiento de texto",
+            version:"1.0",
+            constac:{
+                name:"wenn",
+                email:"florezaraujo@hotmail.com"
+
+            }
+      
+        },
+        servers:[
+            {
+
+            }
+        ]
+      
+      
+
+}
--- src/routers/image.router.ts
+++ src/routers/image.router.ts
@@ -2,11 +2,17 @@
 import express from "express";
 import multer from "multer";
 import { CtrlImage } from "../controllers/image.controllers"; 
+import swaggerUi from "swagger-ui-express";
+import { swaggerOptions } from "../../docs/swagget";
+
+
 
 export const routerImage = express.Router()
     const upload = multer({dest: 'imagess/'  })
 
     routerImage.post('/imagess', upload.single('image'), CtrlImage)
+
+    routerImage.use(`/docs`,swaggerUi.serve, swaggerUi.setup(swaggerOptions))
 
 
 
