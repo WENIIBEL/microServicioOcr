635bfb0e98acea3042eda07494ce3726db7f4629
Marcela <ardilamarcela42@gmail.com>
2024-11-26 20:57:16 -0500

    Prueba

--- src/routers/image.router.ts
+++ src/routers/image.router.ts
@@ -1,17 +1,17 @@
 
 import { Router } from "express";
 import multer from "multer";
-import { Request, Response } from "express";
 import { swaggerOptions } from "../../docs/swagget";
 import swaggerUi from "swagger-ui-express";
 import { CtrlImage } from "../controllers/image.controllers";
 
 const routeri = Router();
 
+//probando
 // Configuración de almacenamiento en memoria
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
-
+console.log("routeri", upload)
 routeri.use(`/docs`,swaggerUi.serve, swaggerUi.setup(swaggerOptions))
 
 // Endpoint para subir imágenes
