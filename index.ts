2497aac4236fd49c5e06dd1783a272d9f1d87ee1
Marcela <ardilamarcela42@gmail.com>
2024-11-27 15:39:12 -0500

    cors

--- index.ts
+++ index.ts
@@ -2,18 +2,19 @@
 import express from "express";
 import { routes } from "./src/routers/index.routes";
 import middleware404 from "./src/middlewares/middlewares";
-
+import cors from "cors";
 
 
 const app = express();
+ app.use(cors())
 const port = 3002;
 
 app.use(express.json());
-app.use(middleware404);
+
 
 app.use("/api/v1", routes())
 
-
+app.use(middleware404);
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en el puerto ${port}`);
