9a52416ea9115b3f4d368984f0ac34ddc93eeddb
Marcela <ardilamarcela42@gmail.com>
2024-11-27 13:46:54 -0500

    Modificando el data-source

--- src/repositories/config/data-source.ts
+++ src/repositories/config/data-source.ts
@@ -2,10 +2,10 @@ import mysql from "mysql2/promise"
 import  config from "config"
 
 const configOptions = {
-    host: config.get<string>("HOST"),
-    port: config.get<number>("PORT"),
-    user: config.get<string>("USER"),
-    password: config.get<string>("PASSWORD"),
+    host: config.get<string>("DB_HOST"),
+    port: config.get<number>("DB_PORT"),
+    user: config.get<string>("DB_USER"),
+    password: config.get<string>("DB_PASSWORD"),
     database: config.get<string>("DATABASE"),
     
   };
